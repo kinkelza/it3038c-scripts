ddca3cc77f6b920e1c4cb8012dd732bca15c7b06
kinkelza <kinkelza@mail.uc.edu>
2023-01-28 12:54:08 -0500

    Final powershell

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -6,9 +6,12 @@ $IP = getIP
 $USER = $env:USERNAME
 $HOST2 =  $env:COMPUTERNAME
 $VERSION = $PSVersionTable.PSVersion
-$DATE = Get-Date -DisplayHint Date
+$DATE = Get-Date -Format "MM-dd-yyyy"
 
 $BODY = "This machine's IP is $IP, User is $USER, Host is $HOST2, Powershell Version is $VERSION, Today Is $DATE"
 
 
-Write-Host($BODY)
+# Write-Host($BODY)
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "zkinkelaar@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
+
+
